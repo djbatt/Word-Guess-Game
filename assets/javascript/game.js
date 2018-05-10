4439c744b92c8cd30405152a315a5ef0e15f184e
djbatt <djbatt@live.com>
2018-05-10 19:14:50 -0400

    began work on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+//This is my hangman game
+//Its themed around Grateful Dead songs, they play while you are guessing for it
+//There are seven songs in total
+var songList = ["Brown Eyed Woman", "Mississippi Half Step Toodeloo", "Fire On The Mountain", "Shakedown Street", "Scarlet Begonias", "Casey Jones", "Sugar Magnolia"];
