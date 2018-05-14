0264999744eb5c7d8ec13f2ddd3dd421e5b27adb
djbatt <djbatt@live.com>
2018-05-14 16:01:25 -0400

    added win and loss scenarios, as well as more detailed comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,50 @@
 const maxAttempts = 12;
-var wordList = ["deadhead", "terrapin", "jerry", "tiedye"];
+var wordList = ["deadhead", "terrapin", "jerry", "tiedye", "stealie", "skeleton"];
 var userWins = 0;
+var userLosses = 0;
 var userAttempts = 0;
 var lettersGuessed = [];
 var wordChoice = [];
 var currentWordIndex;
 
 function startGame() {
-    userAttempts = maxAttempts; // sets the user attempts to the max, it will lower from here.
 
-    document.getElementById("totalWins").innerText = userWins; // sets the html user wins to the amount of wins defined in js.
-    document.getElementById("remainingGuesses").innerText = userAttempts; // sets the html user attempts to the amount of attempts in js.
-    document.getElementById("guessedLetters").innerText = lettersGuessed; // sets the guessed letter to our lettersGuessed variable
-    
-    userAttempts = maxAttempts; // set the amount of user attempts to the max
-    currentWordIndex = Math.floor(Math.random() * (wordList.length)); // Makes the current word chosen at random.\
+    pageStats();
+    winGame();
+    loseGame();
 
-    for (var i = 0; i < wordList[currentWordIndex].length; i++) { // loop this statement
-        wordChoice.push("_"); // build the guesssing word from the length of a random word.
+    currentWordIndex = Math.floor(Math.random() * (wordList.length)); // Creates a number to represent the currentWordIndex
+
+    for (var i = 0; i < wordList[currentWordIndex].length; i++) {
+        wordChoice.push("_"); // Picks the word according to the index, and then pushes underscores according to the length of the word picked.
     }
 
-    for (var i = 0; i < wordChoice.length; i++) { // loop this statement
+    for (var i = 0; i < wordChoice.length; i++) { //Sets the currentWord ID elements text to be equal to the wordChoice
         document.getElementById("currentWord").innerText += wordChoice[i];
     }
 
+    console.log(currentWordIndex);
+    console.log(wordChoice);
+
 }
 
-function displayFunction() {
+function pageStats() {
+    userAttempts = maxAttempts; // sets the user attempts to the max, it will lower from here.
+    document.getElementById("totalWins").innerText = userWins; // sets the html user wins to the amount of wins defined in js.
+    document.getElementById("totalLosses").innerText = userLosses; // sets the html user wins to the amount of wins defined in js.
+    document.getElementById("remainingGuesses").innerText = userAttempts; // sets the html user attempts to the amount of attempts in js.
+    //document.getElementById("guessedLetters").innerText = lettersGuessed; // sets the guessed letter to our lettersGuessed variable
 
+}
+
+function winGame() {
+    if (wordChoice.indexOf("_") === -1) {
+        userWins++;
+    }
+}
+
+function loseGame() {
+    if (remainingGuesses <= 0) {
+        userLosses++;
+    }
 }
