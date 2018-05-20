d2e2f6f28f458cbc94411c21c93baffce385b55e
djbatt <djbatt@live.com>
2018-05-19 20:23:45 -0400

    tweaks to js, added user object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 var wordList = ["deadhead", "terrapin", "jerry", "tiedye", "stealie", "skeleton"];
 var selectedWord;
 var wordChoice = [];
-
-var userWins = 0;
-var userLosses = 0;
-var userAttempts = 10;
 var lettersGuessed = [];
-var letterPos = [];
+
+var user = {
+    wins: 0,
+    losses: 0,
+    attempts: 12,
+}
 
 function startGame() {
-
+    
     selectedWord = Math.floor(Math.random() * (wordList.length)); // selectedWord variable is now equal to a random number value selected from the wordList array.
 
     for (var i = 0; i < wordList[selectedWord].length; i++) { // loop until i is equal to the length of the selected word
@@ -26,13 +27,12 @@ function updatePage() {
     for (var i = 0; i < wordChoice.length; i++) { // loop until i is equal to the length of the wordChoice variable.
         document.getElementById("currentWord").innerText += wordChoice[i]; //change the inner text of the current to be the wordChoice array.
     }
-    document.getElementById("totalWins").innerText = userWins; // sets the html user wins to the amount of wins defined in js.
-    document.getElementById("totalLosses").innerText = userLosses; // sets the html user wins to the amount of wins defined in js.
-    document.getElementById("remainingGuesses").innerText = userAttempts; // sets the html user attempts to the amount of attempts in js
-    //document.getElementById("pastGuess").innerText = lettersGuessed; // sets the html user attempts to the amount of attempts in js
+    document.getElementById("totalWins").innerText = user.wins;
+    document.getElementById("totalLosses").innerText = user.losses;
+    document.getElementById("remainingGuesses").innerText = user.attempts;
 }
 
-document.onkeyup = function(event) { //Pressing a key is now the function, defined by the event parameter
+document.onkeyup = function() {
     if(event.keyCode >= 65 && event.keyCode <=90) { // if you press keys a-z run the script below.
         updatePage();
     }
